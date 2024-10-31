f4847e3448cf685ce10849272c215bd9f0c7d784
João Lee <joaumlimaum@gmail.com>
2024-10-31 16:55:13 -0300

    fix/feat: downgrade project packages versions and create first migration

--- Services/GeekShopping.ProductAPI/Program.cs
+++ Services/GeekShopping.ProductAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-var connection = builder.Configuration.GetConnectionString("MySQLConnectionString");
+var connection = builder.Configuration["MySQLConnectionString"];
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MySQLContext>
